135e184516da0bc25d61088fe96bff3a16def269
TimoSchmechel <timo@schmechel.com>
2016-02-19 11:43:35 +1100

    Infinite Level creation started

---
+++ GravJump/Assets/Scripts/LevelCreator.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelCreator : MonoBehaviour {
+
+    public GameObject[] levelPieces;
+
+	// Use this for initialization
+	void Awake () { 
+	}
+
+    void Start()
+    {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ GravJump/Assets/Scripts/LevelPiece.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelPiece : MonoBehaviour {
+
+    public bool startDirectionUp;
+    public bool endDirectionUp;
+    public Transform endPos;
+
+    private GameObject checkpoint;
+
+	void Awake () {
+        endPos = transform.FindChild("EndPos");
+        checkpoint = transform.FindChild("Checkpoint").gameObject;
+        checkpoint.SetActive(false);
+	}
+
+    public void MakeCheckpoint()
+    {
+        checkpoint.SetActive(true);
+    }
+
+}
